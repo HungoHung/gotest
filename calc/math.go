b1b294732ef04a06f800a0bfd7da65e2107d5643
Hungo Hung <hungo.hung@prophetstor.com>
2020-12-16 16:25:51 +0800

    Add accepts variadic arguments

--- calc/math.go
+++ calc/math.go
@@ -1,5 +1,12 @@
 package calc
 
-func Add(i int, j int) int {
-	return i + j
+// Add - return sum of two integers	
+func Add(numbers ...int	) int {
+	sum := 0
+
+	for _, num := range numbers{
+		sum = sum + num
+	}
+
+	return sum
 }
